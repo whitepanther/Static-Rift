6bad656232c95862a28d2c7c03ad96cc717e5ae2
whitepanther <black_panthercat@hotmail.com>
2013-07-04 00:51:52 -0400

    Test

--- SRift_common/whitepanther/SRift/StaticRift.java
+++ SRift_common/whitepanther/SRift/StaticRift.java
@@ -6,33 +6,41 @@ import whitepanther.SRift.lib.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-
-@Mod(
-        modid= Reference.MOD_ID,
-        name= Reference.MOD_NAME, 
-        version= Reference.VERSION_NUMBER)
-
-@NetworkMod(clientSideRequired = true, serverSideRequired = false)
-
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER)
+// @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class StaticRift
 {
-    
+
+    @PreInit
+    public void preinit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Init
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
     @Instance(Reference.MOD_ID)
     public static StaticRift instance;
-    
-    
+
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy; 
-    
-    
-    @Init
-    public void load(FMLInitializationEvent event)
-    {
-        proxy.registerRenderers();
-    }
-    
+    public static CommonProxy proxy;
+
 }
--- SRift_common/whitepanther/SRift/lib/Reference.java
+++ SRift_common/whitepanther/SRift/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference
     public static final String MOD_ID = "SRift";
     public static final String MOD_NAME = "Static Rift";
     public static final String VERSION_NUMBER = "0.0.0";
-    public static final String SERVER_PROXY_CLASS = "com.whitepanther.SRift.core.proxy.CommonProxy";
-    public static final String CLIENT_PROXY_CLASS = "com.whitepanther.SRift.core.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "whitepanther.SRift.core.proxy.CommonProxy";
+    public static final String CLIENT_PROXY_CLASS = "whitepanther.SRift.core.proxy.ClientProxy";
    
 }
